81abc79e13b825f429d046f9689265b7624b1a8f
Mohmad89 <mohmadadarbeh7@gmail.com>
2022-03-22 14:58:03 +0300

    Lab12 Uploaded

---
+++ demo/src/main/java/com/example/demo/infrastructure/AlbumsRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.infrastructure;
+
+import com.example.demo.domain.Albums;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlbumsRepository  extends CrudRepository <Albums, Integer> {
+
+}
